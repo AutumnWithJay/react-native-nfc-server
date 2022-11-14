6d2bf635280405a7c61bc58422b771238b3208c9
Park Joon Woo <devpark.0x1c@gmail.com>
2022-11-14 11:20:37 +0900

    fix: 중복 직원등록 방지

--- src/controller/EmployeeController.ts
+++ src/controller/EmployeeController.ts
@@ -15,7 +15,11 @@ export const getEmployeeList = async (
   const { aptId } = req.query;
   const employees: string[] = [];
 
-  const employeeRef = await db.collection(aptId).get();
+  const employeeRef = await db
+    .collection(aptId)
+    .doc('employee')
+    .collection('list')
+    .get();
 
   employeeRef.forEach((doc) => {
     const employee = doc.data();
@@ -23,9 +27,13 @@ export const getEmployeeList = async (
   });
 
   if (employees.length === 0) {
-    return res.status(400).send('데이터를 찾을수 없습니다');
+    return res.status(200).json({
+      message: '데이터를 찾을수 없습니다',
+    });
   } else {
-    return res.status(200).json(employees);
+    return res.status(200).json({
+      data: employees,
+    });
   }
 };
 
@@ -33,17 +41,46 @@ export const registerEmployee = async (
   req: express.Request,
   res: express.Response,
 ) => {
+  let isExist: boolean = false;
   const employeeData: typeGuard = req.body;
-  const { aptId, id, name, phone } = employeeData;
+  const { aptId, name, phone } = employeeData;
 
-  const fbWriteResponse = await db.collection(aptId).doc(id).set({
-    id,
-    name,
-    phone,
-    registerDate: new Date(),
+  const employeeRef = await db
+    .collection(aptId)
+    .doc('employee')
+    .collection('list')
+    .where('name', '==', name)
+    .where('phone', '==', phone)
+    .get();
+
+  employeeRef.forEach((doc) => {
+    return doc.data() ? (isExist = true) : (isExist = false);
   });
 
-  console.log(fbWriteResponse);
+  if (!!isExist) {
+    res.status(200).json({
+      message: '이미 등록되어있는 직원입니다',
+    });
+  } else {
+    const fbWriteResponse = await db
+      .collection(aptId)
+      .doc('employee')
+      .collection('list')
+      .doc()
+      .set({
+        name,
+        phone,
+        registerDate: new Date(),
+      });
 
-  res.status(201).send('직원이 등록되었습니다');
+    if (fbWriteResponse) {
+      res.status(201).json({
+        message: '직원이 등록되었습니다',
+      });
+    } else {
+      res.status(400).json({
+        message: '등록에 실패하였습니다',
+      });
+    }
+  }
 };
