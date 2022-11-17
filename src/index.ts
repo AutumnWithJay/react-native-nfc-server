457d57bd04814d80559cae954d5a3994c39887ca
Park Joon Woo <devpark.0x1c@gmail.com>
2022-11-17 14:10:31 +0900

    feat: cors 적용

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import EmployeeRouter from './routes/EmployeeRouter';
 import PatrolRouter from './routes/PatrolRouter';
 
+const cors = require('cors');
 const app = express();
 const PORT = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/', EmployeeRouter);
 app.use('/patrol', PatrolRouter);
